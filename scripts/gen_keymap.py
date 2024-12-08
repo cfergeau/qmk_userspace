3d6aadd50d8996fe830c2ebc5409929b2e44a5cb
Christophe Fergeau <cfergeau@redhat.com>
2024-12-08 14:07:55 +0100

    Add 1DK and Compose to QWERTY and Dvorak layouts

--- scripts/gen_keymap.py
+++ scripts/gen_keymap.py
@@ -1069,13 +1069,13 @@ keymaps = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT, _______, MO(NumFn),XXXXXXX,KC_SPC,  MO(Sym), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+        KC_LCTL, KC_LGUI, KC_LALT, _______, MO(NumFn),XXXXXXX,KC_SPC,  MO(Sym), OSL(DK), KC_DOWN, KC_UP,   KC_RCTL
         """,
         Mode.Dvorak: """
         KC_TAB,  KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
         KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
         KC_LSFT, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT, _______, MO(NumFn),XXXXXXX,KC_SPC,  MO(Sym), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
+        KC_LCTL, KC_LGUI, KC_LALT, _______, MO(NumFn),XXXXXXX,KC_SPC,  MO(Sym), OSL(DK), KC_DOWN, KC_UP,   KC_RCTL
         """,
         Mode.NumFn: """
         KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
